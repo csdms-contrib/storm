086871cf0ded45a0692bab11bbd99ee988e4f3e3
Mark Piper <mark.piper@colorado.edu>
2015-02-13 12:11:30 -0700

    Stub out files for storm BMI

---
+++ bmi/stormc.pc.cmake
@@ -0,0 +1,5 @@
+Name: Storm
+Description: Computes the windfield for a cyclone
+Version: ${STORM_VERSION}
+Libs: -L${CMAKE_INSTALL_PREFIX}/lib -lbmi_stormc
+Cflags: -I${CMAKE_INSTALL_PREFIX}/include
