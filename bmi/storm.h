a5c77c208cce52708b2781499d70b47b04d7da99
Mark Piper <mark.piper@colorado.edu>
2015-02-17 14:19:30 -0700

    Don't use C++-style comments

--- bmi/storm.h
+++ bmi/storm.h
@@ -5,16 +5,15 @@
 extern "C" {
 #endif
 
-
 typedef struct {
-  int t;              // current time step
-  int t_end;          // last time step
-  int t_next;         // next time step for which data are read
-  int shape[2];       // grid dimensions 
-  double spacing[2];  // grid spacing [m]
-  int center[2];      // grid cell of storm center
-  double **wdir;      // wind direction
-  double **wspd;      // wind speed
+  int t;              /* current time step */
+  int t_end;          /* last time step */
+  int t_next;         /* next time step for which data are read */
+  int shape[2];       /* grid dimensions */
+  double spacing[2];  /* grid spacing [m] */
+  int center[2];      /* grid cell of storm center */
+  double **wdir;      /* wind direction */
+  double **wspd;      /* wind speed */
 } StormModel;
 
 extern StormModel * storm_from_default (void);
