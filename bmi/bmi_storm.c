fbba4c242cf91fffd449ad3fc234059972d4a491
Mark Piper <mark.piper@colorado.edu>
2015-03-01 13:42:45 -0700

    Update bmi_storm.c so it compiles

--- bmi/bmi_storm.c
+++ bmi/bmi_storm.c
@@ -1,11 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#include <bmi/bmi.h>
 
 #include "bmi_storm.h"
 #include "storm.h"
 
 
-int
+static int
+Get_start_time (void *self, double * time)
+{
+  if (time) {
+    *time = 0.;
+    return BMI_SUCCESS;
+  }
+  else {
+    return BMI_FAILURE;
+  }
+}
+
+
+static int
+Get_end_time (void *self, double * time)
+{
+  *time = ((StormModel *)self)->t_end;
+  return BMI_SUCCESS;
+}
+
+
+static int
+Get_time_step (void *self, double * dt)
+{
+  *dt = ((StormModel *)self)->dt;
+  return BMI_SUCCESS;
+}
+
+
+static int
+Get_time_units (void *self, char * units)
+{
+  strncpy (units, "-", BMI_MAX_UNITS_NAME);
+  return BMI_SUCCESS;
+}
+
+
+static int
+Get_current_time (void *self, double * time)
+{
+  *time = ((StormModel *)self)->t;
+  return BMI_SUCCESS;
+}
+
+
+static int
 Initialize (const char *file, void **handle)
 {
   StormModel *self = NULL;
@@ -24,7 +72,7 @@ Initialize (const char *file, void **handle)
 }
 
 
-int
+static int
 Update (void *self)
 {
   storm_advance_time ((StormModel *) self);
@@ -77,7 +125,7 @@ Update_until (void *self, double t)
 }
 
 
-int
+static int
 Finalize (void *self)
 {
   if (self)
@@ -87,46 +135,53 @@ Finalize (void *self)
 }
 
 
-static int
-Get_start_time (void *self, double * time)
+BMI_Model *
+Construct_heat_bmi(BMI_Model *model)
 {
-  if (time) {
-    *time = 0.;
-    return BMI_SUCCESS;
+  if (model) {
+    model->self = NULL;
+
+    model->initialize = Initialize;
+    model->update = Update;
+    model->update_until = Update_until;
+    model->update_frac = Update_frac;
+    model->finalize = Finalize;
+    /* model->run_model = NULL; */
+
+    /* model->get_component_name = Get_component_name; */
+    /* model->get_input_var_name_count = Get_input_var_name_count; */
+    /* model->get_output_var_name_count = Get_output_var_name_count; */
+    /* model->get_input_var_names = Get_input_var_names; */
+    /* model->get_output_var_names = Get_output_var_names; */
+
+    /* model->get_var_type = Get_var_type; */
+    /* model->get_var_units = Get_var_units; */
+    /* model->get_var_rank = Get_var_rank; */
+    /* model->get_var_size = Get_var_size; */
+    /* model->get_var_nbytes = Get_var_nbytes; */
+    model->get_current_time = Get_current_time;
+    model->get_start_time = Get_start_time;
+    model->get_end_time = Get_end_time;
+    model->get_time_units = Get_time_units;
+    model->get_time_step = Get_time_step;
+
+    /* model->get_value = Get_value; */
+    /* model->get_value_ptr = Get_value_ptr; */
+    /* model->get_value_at_indices = Get_value_at_indices; */
+
+    /* model->set_value = Set_value; */
+    /* model->set_value_ptr = NULL; */
+    /* model->set_value_at_indices = Set_value_at_indices; */
+
+    /* model->get_grid_type = Get_grid_type; */
+    /* model->get_grid_shape = Get_grid_shape; */
+    /* model->get_grid_spacing = Get_grid_spacing; */
+    /* model->get_grid_origin = Get_grid_origin; */
+
+    /* model->get_grid_x = NULL; */
+    /* model->get_grid_y = NULL; */
+    /* model->get_grid_z = NULL; */
   }
-  else {
-    return BMI_FAILURE;
-  }
-}
 
-
-static int
-Get_end_time (void *self, double * time)
-{
-  *time = ((StormModel *)self)->t_end;
-  return BMI_SUCCESS;
-}
-
-
-static int
-Get_time_step (void *self, double * dt)
-{
-  *dt = ((StormModel *)self)->dt;
-  return BMI_SUCCESS;
-}
-
-
-static int
-Get_time_units (void *self, char * units)
-{
-  strncpy (units, "-", BMI_MAX_UNITS_NAME);
-  return BMI_SUCCESS;
-}
-
-
-static int
-Get_current_time (void *self, double * time)
-{
-  *time = ((StormModel *)self)->t;
-  return BMI_SUCCESS;
+  return model;
 }
