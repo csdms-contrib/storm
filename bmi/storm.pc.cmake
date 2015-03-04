01b488f3d5f4e11b6a667aa0744ab7ccdc7a4e7a
Mark Piper <mark.piper@colorado.edu>
2015-03-04 12:02:37 -0700

    Rename storm pkgconfig file

---
+++ bmi/storm.pc.cmake
@@ -0,0 +1,5 @@
+Name: Storm
+Description: Computes the horizontal wind field for a cyclone
+Version: ${STORM_VERSION}
+Libs: -L${CMAKE_INSTALL_PREFIX}/lib -lbmi_storm
+Cflags: -I${CMAKE_INSTALL_PREFIX}/include
--- bmi/stormc.pc.cmake
+++
@@ -1,5 +0,0 @@
-Name: Storm
-Description: Computes the windfield for a cyclone
-Version: ${STORM_VERSION}
-Libs: -L${CMAKE_INSTALL_PREFIX}/lib -lbmi_stormc
-Cflags: -I${CMAKE_INSTALL_PREFIX}/include
