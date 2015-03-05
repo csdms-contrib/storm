bfe9318a3a4da456f4cbd1e17776053a2fc704c6
Mark Piper <mark.piper@colorado.edu>
2015-03-05 14:06:04 -0700

    Add set_value and grid_info unit tests

---
+++ testing/test_grid_info.c
@@ -0,0 +1,123 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "bmi_storm.h"
+#include "bmilib.h"
+#include "storm.h"
+
+
+void print_var_info (BMI_Model *model, const char *var);
+
+int
+main (void)
+{
+  BMI_Model *model = (BMI_Model*)malloc (sizeof(BMI_Model));
+
+  Construct_storm_bmi(model);
+
+  BMI_Initialize (model, NULL);
+
+  {
+    char name[BMI_MAX_COMPONENT_NAME];
+    BMI_Get_component_name (model, name);
+    fprintf (stdout, "%s\n", name);
+  }
+
+  { /* Input items */
+    int n_names;
+    char **names = NULL;
+    int i;
+
+    BMI_Get_input_var_name_count(model, &n_names);
+
+    names = (char**) malloc (sizeof(char*) * n_names);
+    for (i = 0; i<n_names; i++)
+      names[i] = (char*) malloc (sizeof (char) * BMI_MAX_VAR_NAME);
+
+    BMI_Get_input_var_names (model, names);
+    fprintf (stdout, "\n");
+    fprintf (stdout, "Input Variables\n");
+    fprintf (stdout, "===============\n");
+    fprintf(stdout, "Number of variables: %d\n", n_names); fflush(stdout);
+    for (i = 0; i<n_names; i++)
+      print_var_info (model, names[i]);
+
+    for (i = 0; i<n_names; i++)
+      free (names[i]);
+    free (names);
+  }
+
+  { /* Output items */
+    int n_names;
+    char **names = NULL;
+    int i;
+
+    BMI_Get_output_var_name_count(model, &n_names);
+
+    names = (char**) malloc (sizeof(char*) * n_names);
+    for (i = 0; i<n_names; i++)
+      names[i] = (char*) malloc (sizeof (char) * BMI_MAX_VAR_NAME);
+
+    BMI_Get_output_var_names (model, names);
+    fprintf (stdout, "\n");
+    fprintf (stdout, "Output Variables\n");
+    fprintf (stdout, "================\n");
+    fprintf(stdout, "Number of variables: %d\n", n_names); fflush(stdout);
+    for (i = 0; i<n_names; i++)
+      print_var_info (model, names[i]);
+
+    for (i = 0; i<n_names; i++)
+      free (names[i]);
+    free (names);
+  }
+
+  BMI_Finalize (model);
+
+  free (model);
+
+  return EXIT_SUCCESS;
+}
+
+void
+print_var_info (BMI_Model *model, const char *var)
+{
+  char type[2048];
+  char units[BMI_MAX_UNITS_NAME];
+  int n_dims = 0;
+
+  BMI_Get_var_type (model, var, type);
+  BMI_Get_var_units (model, var, units);
+  BMI_Get_var_rank (model, var, &n_dims);
+
+  fprintf (stdout, "\n");
+  fprintf (stdout, "Name: %s\n", var);
+  fprintf (stdout, "Type: %s\n", type);
+  fprintf (stdout, "Units: %s\n", units);
+  fprintf (stdout, "Rank: %d\n", n_dims);
+
+  {
+    int *shape = NULL;
+    double *spacing = NULL;
+    double *origin = NULL;
+
+    if (n_dims > 1) {
+      shape = (int*) malloc (sizeof (int)*n_dims);
+      spacing = (double*) malloc (sizeof (double)*n_dims);
+      origin = (double*) malloc (sizeof (double)*n_dims);
+
+      BMI_Get_grid_shape (model, var, shape);
+      BMI_Get_grid_spacing (model, var, spacing);
+      BMI_Get_grid_origin (model, var, origin);
+
+      fprintf (stdout, "Dimension: %d x %d\n", shape[0], shape[1]);
+      fprintf (stdout, "Resolution: %f x %f\n", spacing[0], spacing[1]);
+      fprintf (stdout, "Corner: %f x %f\n", origin[0], origin[1]);
+    }
+
+    free (origin);
+    free (spacing);
+    free (shape);
+  }
+
+  return;
+}
---
+++ testing/test_set_value.c
@@ -0,0 +1,126 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "bmi_storm.h"
+#include "bmilib.h"
+
+
+void print_var_values (void *model, const char *var_name);
+
+int
+main (void)
+{
+  double *new_vals = NULL;
+  int err = 0;
+  BMI_Model * model = (BMI_Model*)malloc (sizeof(BMI_Model));
+
+  Construct_storm_bmi(model);
+
+  err = BMI_Initialize (model, NULL);
+  if (err)
+    return EXIT_FAILURE;
+
+  {
+    char name[BMI_MAX_COMPONENT_NAME];
+    BMI_Get_component_name (model, name);
+  
+    fprintf (stdout, "%s\n", name);
+  }
+
+  { /* Get grid information */
+    int n_dims;
+    int *shape;
+    int len = 0;
+    int i;
+
+    BMI_Get_var_rank (model, 
+		      "atmosphere_air_flow__east_component_of_velocity", 
+		      &n_dims);
+    shape = (int*) malloc (sizeof (int)*n_dims);
+
+    BMI_Get_grid_shape (model, 
+			"atmosphere_air_flow__east_component_of_velocity", 
+			shape);
+    for (i = 0, len = 1; i < n_dims; i++)
+      len *= shape[i];
+
+    new_vals = (double *)malloc (sizeof (double) * len);
+    for (i = 0; i < len; i++)
+      new_vals[i] = -1;
+
+    free (shape);
+  }
+
+  fprintf (stdout, "Values before set\n");
+  fprintf (stdout, "=================\n");
+  print_var_values (model, "atmosphere_air_flow__east_component_of_velocity");
+
+  BMI_Set_value (model, 
+		 "atmosphere_air_flow__east_component_of_velocity", 
+		 new_vals);
+
+  fprintf (stdout, "Values after set\n");
+  fprintf (stdout, "================\n");
+  print_var_values (model, "atmosphere_air_flow__east_component_of_velocity");
+
+  {
+    int inds[5] = {1, 2, 4, 8, 16};
+    double vals[5] = {11, 22, 44, 88, 1616};
+    double *p;
+    int i;
+
+    BMI_Set_value_at_indices (model, 
+			      "atmosphere_air_flow__east_component_of_velocity",
+			      inds, 5, vals);
+    print_var_values (model, "atmosphere_air_flow__east_component_of_velocity");
+
+    BMI_Get_value_ptr (model, 
+		       "atmosphere_air_flow__east_component_of_velocity", 
+		       (void**)(&p));
+    for (i=0; i<5; i++) {
+      fprintf (stdout, "Checking %d...", inds[i]);
+      if (p[inds[i]] == vals[i])
+        fprintf (stdout, "PASS\n");
+    }
+  }
+
+  free (new_vals);
+
+  BMI_Finalize (model);
+
+  free (model);
+
+  return EXIT_SUCCESS;
+}
+
+void
+print_var_values (void *model, const char *var_name)
+{
+  double *var = NULL;
+  int n_dims = 0;
+  int *shape = NULL;
+
+  BMI_Get_var_rank (model, var_name, &n_dims);
+  shape = (int*) malloc (sizeof (int)*n_dims);
+
+  BMI_Get_grid_shape (model, 
+		      "atmosphere_air_flow__east_component_of_velocity", 
+		      shape);
+  BMI_Get_value_ptr (model, var_name, (void**)(&var));
+
+  fprintf (stdout, "Variable: %s\n", var_name);
+  fprintf (stdout, "Number of dimensions: %d\n", n_dims);
+  fprintf (stdout, "Shape: %d x %d\n", shape[0], shape[1]);
+  fprintf (stdout, "================\n");
+
+  {
+    int i, j;
+    for (i = 0; i < shape[0]; i++) {
+      for (j = 0; j < shape[1]; j++)
+        fprintf (stdout, "%f ", var[i*shape[1]+j]);
+      fprintf (stdout, "\n");
+    }
+  }
+
+  return;
+}
