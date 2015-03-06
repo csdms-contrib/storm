bda7f15955a252fb6d162dd91184752d40be5549
Mark Piper <mark.piper@colorado.edu>
2015-03-05 17:07:05 -0700

    Make a new unit test for variable info
    Split off the code from test_grid_info.c.

--- testing/test_var_info.c
+++ testing/test_var_info.c
@@ -81,39 +81,25 @@ main (void)
 void
 print_var_info (BMI_Model *model, const char *var)
 {
+  char type[BMI_MAX_TYPE_NAME];
+  char units[BMI_MAX_UNITS_NAME];
   int n_dims = 0;
+  int size = 0;
+  int n_bytes = 0;
 
+  BMI_Get_var_type (model, var, type);
+  BMI_Get_var_units (model, var, units);
   BMI_Get_var_rank (model, var, &n_dims);
+  BMI_Get_var_size (model, var, &size);
+  BMI_Get_var_nbytes (model, var, &n_bytes);
 
-  {
-    char type[BMI_MAX_TYPE_NAME];
-    int *shape = NULL;
-    double *spacing = NULL;
-    double *origin = NULL;
-
-    if (n_dims > 1) {
-      shape = (int*) malloc (sizeof (int)*n_dims);
-      spacing = (double*) malloc (sizeof (double)*n_dims);
-      origin = (double*) malloc (sizeof (double)*n_dims);
-
-      BMI_Get_grid_type (model, var, type);
-      BMI_Get_grid_shape (model, var, shape);
-      BMI_Get_grid_spacing (model, var, spacing);
-      BMI_Get_grid_origin (model, var, origin);
-
-      fprintf (stdout, "\n");
-      fprintf (stdout, "Name: %s\n", var);
-      fprintf (stdout, "Grid type: %s\n", type);
-      fprintf (stdout, "Rank: %d\n", n_dims);
-      fprintf (stdout, "Dimension: %d x %d\n", shape[0], shape[1]);
-      fprintf (stdout, "Resolution: %f x %f\n", spacing[0], spacing[1]);
-      fprintf (stdout, "Corner: %f x %f\n", origin[0], origin[1]);
-    }
-
-    free (origin);
-    free (spacing);
-    free (shape);
-  }
+  fprintf (stdout, "\n");
+  fprintf (stdout, "Name: %s\n", var);
+  fprintf (stdout, "Type: %s\n", type);
+  fprintf (stdout, "Units: %s\n", units);
+  fprintf (stdout, "Rank: %d\n", n_dims);
+  fprintf (stdout, "Size: %d\n", size);
+  fprintf (stdout, "Bytes: %d\n", n_bytes);
 
   return;
 }
